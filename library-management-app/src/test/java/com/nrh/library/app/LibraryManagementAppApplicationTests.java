0ed2d325b43870d31d94520f14ee999e363be4d8
nrh674 <n.riccahang674@gmail.com>
2025-02-03 21:17:45 +0400

    [feat]: init 03/02/2025 21:17

---
+++ library-management-app/src/test/java/com/nrh/library/app/LibraryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrh.library.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
